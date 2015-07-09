6c04ad77858f41643f9eb7e21b78ba3cb7d643cb
Olivia Ross <ross.mollie@gmail.com>
2015-07-09 13:29:31 -0400

    Edited lecture notes

---
+++ Mirror.spritebuilder/Hero.swift
@@ -0,0 +1,13 @@
+//
+//  Hero.swift
+//  Mirror
+//
+//  Created by Olivia Ross on 7/9/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class Hero: CCSprite {
+   
+}
---
+++ Mirror.spritebuilder/Source/Gameplay.swift
@@ -0,0 +1,13 @@
+//
+//  Gameplay.swift
+//  Mirror
+//
+//  Created by Olivia Ross on 7/9/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class Gameplay: CCNode {
+   
+}
