1457061721e9d907ee03a4797af68f48c5929300
Olivia Ross <ross.mollie@gmail.com>
2015-07-13 14:54:51 -0400

    walking at a reasonable pace

--- Mirror.spritebuilder/Source/Gameplay.swift
+++ Mirror.spritebuilder/Source/Gameplay.swift
@@ -19,28 +19,18 @@ class Gameplay: CCScene{
     }
     
     func moveLeft() {
-        hero.position.x -= 10
+        hero.position.x -= 20
         hero.animationManager.runAnimationsForSequenceNamed("WalkingRight")
-
     }
     
     func moveRight() {
-        hero.position.x += 10
+        hero.position.x += 20
         hero.animationManager.runAnimationsForSequenceNamed("WalkingLeft")
     }
     
     func jump() {
         hero.animationManager.runAnimationsForSequenceNamed("Jumping")
-        hero.physicsBody.applyImpulse(CGPoint(x: 0, y: 200))
+        hero.physicsBody.applyImpulse(CGPoint(x: 0, y: 100))
     }
     
-    //CAN'T TELL IF THE JEWEL IS AN ITEM OR A CONTROL
-        //MAKE THE GROUND TALLER, PUT THE JEWEL IN THE GROUND
-        //MAKE THE JUMP BUTTON MORE OBVIOUS, MAYBE AN "UP" ARROW?
-    //IF YOU'RE FACING LEFT, YOU TURN AROUND WHEN YOU JUMP
-    //DON'T LET THEM WALK OFF THE WORLD
-    //TOO SLOW
-    //PLAYER SHOULD KEEP WALKING WHEN BUTTON IS HELD
-    //ANIMATION CONTINUES AFTER YOU'VE STOPPED PRESSING THE BUTTON
-
 }
