87c26ffba825fac1d02c52f85fa8909c834a1f42
Olivia Ross <ross.mollie@gmail.com>
2015-07-13 16:40:53 -0400

    Hero now continues to walk until player lets go of the button

--- Mirror.spritebuilder/Source/Gameplay.swift
+++ Mirror.spritebuilder/Source/Gameplay.swift
@@ -12,6 +12,10 @@ class Gameplay: CCScene{
     
     weak var hero: Hero!
     weak var gamePhysicsNode: CCPhysicsNode!
+    weak var rightButton: CCButton!
+    weak var leftButton: CCButton!
+    var rightButtonPreviousState = false
+    var leftButtonPreviousState = false
     
     func didLoadFromCCB(){
         userInteractionEnabled = true
@@ -19,18 +23,48 @@ class Gameplay: CCScene{
     }
     
     func moveLeft() {
-        hero.position.x -= 20
-        hero.animationManager.runAnimationsForSequenceNamed("WalkingRight")
-    }
-    
-    func moveRight() {
-        hero.position.x += 20
+        hero.position.x -= 0.5
         hero.animationManager.runAnimationsForSequenceNamed("WalkingLeft")
     }
     
-    func jump() {
-        hero.animationManager.runAnimationsForSequenceNamed("Jumping")
-        hero.physicsBody.applyImpulse(CGPoint(x: 0, y: 100))
+    func moveRight() {
+        hero.position.x += 0.5
+        hero.animationManager.runAnimationsForSequenceNamed("WalkingRight")
     }
     
+    func jump() {
+            hero.animationManager.runAnimationsForSequenceNamed("Jumping")
+            hero.physicsBody.applyImpulse(CGPoint(x: 0, y: 100))
+    }
+    
+    override func update(delta: CCTime) {
+        if rightButton.highlighted {
+            hero.position.x += 0.5
+            
+            if rightButtonPreviousState == false {
+                hero.animationManager.runAnimationsForSequenceNamed("WalkingRight")
+            }
+            
+        } else {
+            if rightButtonPreviousState == true{
+                hero.animationManager.runAnimationsForSequenceNamed("Idling")
+            }
+        }
+        
+        if leftButton.highlighted {
+            hero.position.x -= 0.5
+            
+            if leftButtonPreviousState == false {
+                hero.animationManager.runAnimationsForSequenceNamed("WalkingLeft")
+            }
+            
+        } else {
+            if leftButtonPreviousState == true{
+                hero.animationManager.runAnimationsForSequenceNamed("Idling")
+            }
+        }
+        
+        rightButtonPreviousState = rightButton.highlighted
+        leftButtonPreviousState = leftButton.highlighted
+    }
 }
